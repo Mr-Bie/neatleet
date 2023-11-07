2ecdbd8adc493dfe7d592acdfc306ddf68ee6b81
Mr-Bie <mrb800424@gmail.com>
2023-11-07 10:33:37 +0330

    FEAT: array duplicate finder func added

--- 36-valid-sudoku/index.js
+++ 36-valid-sudoku/index.js
@@ -1,1 +1,10 @@
 var isValidSudoku = function (board) {};
+
+var noDuplicate = function (array) {
+  let map = new Map();
+  for (let i = 0; i < array.length; i++) {
+    if (map.has(array[i])) return false;
+    else map.set(array[i], 1);
+  }
+  return true;
+};
